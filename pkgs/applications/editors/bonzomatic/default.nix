8178146a611c7886119d67156ed7d65d5bb0367e
OPNA2608 <christoph.neidahl@gmail.com>
2021-04-18 23:19:21 +0200

    wip bonzomatic

--- pkgs/applications/editors/bonzomatic/default.nix
+++ pkgs/applications/editors/bonzomatic/default.nix
@@ -19,8 +19,8 @@ stdenv.mkDerivation rec {
     # CMakeFiles/bonzomatic.dir/src/platform_common/FFT.cpp.o: undefined reference to symbol 'dlclose@@GLIBC_2.2.5'
     # libdl.so.2: error adding symbols: DSO missing from command line
     substituteInPlace CMakeLists.txt \
-      --replace "pkg_search_module" "pkg_check_modules" \
-      --replace "PLATFORM_LIBS GL asound fontconfig" "PLATFORM_LIBS GL asound fontconfig dl"
+      --replace "PLATFORM_LIBS GL asound fontconfig" "PLATFORM_LIBS GL asound fontconfig dl" \
+      --replace "if (APPLE OR WIN32)" "if (WIN32)"
   '';
 
   nativeBuildInputs = [ cmake pkg-config stb miniaudio ];
